aaf3e02059cbed8b892b7ae2533a2e0651209893
Akash Das <akashd595@gmail.com>
2022-09-22 10:26:11 +0530

    ADDED: regex check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,10 +22,16 @@ export class AppComponent {
     window.open(this.url, "blank");
   }
   userName: string = "";
-
+  nameError: string = "";
   onInput($event: any) {
     console.log("Event changed ", $event.data);
-    
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect";
+  
   }
 
 
